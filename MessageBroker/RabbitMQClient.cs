a497b14d5298ef67a93f53d003d9d003dc1c9b2c
Immanuel <nowpertimmanuel@gmail.com>
2023-07-05 08:02:22 +0530

    fix:fixed dns

--- MessageBroker/RabbitMQClient.cs
+++ MessageBroker/RabbitMQClient.cs
@@ -36,7 +36,7 @@ namespace PaymentService.MessageBroker
             //Here we specify the Rabbit MQ Server. we use rabbitmq docker image and use it
             _connectionFactory = new ConnectionFactory
             {
-                HostName = "localhost"
+                HostName = "message-queue"
             };
             try
             {
